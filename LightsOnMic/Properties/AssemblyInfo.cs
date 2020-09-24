7ad49060fbe7b9a09a8bbc153007aea435806473
J. Schlackman <james@schlackman.org>
2020-09-24 17:20:46 -0400

    Tidy up assembly information
    Additional info was causing a ridiculous path to be generated for the user settings file.

--- LightsOnMic/Properties/AssemblyInfo.cs
+++ LightsOnMic/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyTitle("LightsOnMic")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("J. Schlackman")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LightsOnMic")]
 [assembly: AssemblyCopyright("Copyright © 2020 J. Schlackman")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
